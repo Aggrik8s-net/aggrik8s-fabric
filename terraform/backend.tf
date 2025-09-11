8ad7128b44c55c216e36f483a67d4195f3326666
david.shea <dave@aggrik8s.net>
2025-09-11 14:49:43 -0400

    Housekeeping on DO Spaces buckets.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,4 +1,8 @@
 //  TEST bucket - verified to work
+import {
+  to = digitalocean_spaces_bucket.foobar-aggrik8s-fabric
+  id = "nyc3,foobar-aggrik8s-fabric" # Replace with your bucket's region and name
+} 
 resource "digitalocean_spaces_bucket" "foobar-aggrik8s-fabric" {
   name   = "foobar-aggrik8s-fabric"
   region = "nyc3"
@@ -17,16 +21,16 @@ resource "digitalocean_spaces_bucket" "foobar-aggrik8s-fabric" {
 // The attributes commented out are READ ONLY, get from state as shown above.
 //
 
-resource "digitalocean_spaces_bucket" "aggrik8s-fabric" {
-  // bucket_domain_name = "aggrik8s-cluster.nyc3.digitaloceanspaces.com"
-  // endpoint           = "nyc3.digitaloceanspaces.com"
-  force_destroy      = false
-  // id                 = "aggrik8s-cluster"
-  name               = "aggrik8s-fabric"
-  region             = "nyc3"
-  // urn                = "do:space:aggrik8s-cluster"
-
-  versioning {
-    enabled = true
-  }
-}
+// resource "digitalocean_spaces_bucket" "aggrik8s-fabric" {
+//   // bucket_domain_name = "aggrik8s-cluster.nyc3.digitaloceanspaces.com"
+//   // endpoint           = "nyc3.digitaloceanspaces.com"
+//   force_destroy      = false
+//   // id                 = "aggrik8s-cluster"
+//   name               = "aggrik8s-fabric"
+//   region             = "nyc3"
+//   // urn                = "do:space:aggrik8s-cluster"
+// 
+//   versioning {
+//     enabled = true
+//   }
+// }
